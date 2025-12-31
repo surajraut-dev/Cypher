7b3fee516fd1dea71a68fbb4aeebf4da1a26daa2
Suraj Raut <surajraut.dev@gmail.com>
2025-12-31 20:58:32 +0530

    Updated Swiper with fade effect and smooth transitions v8

--- pages/mainmenu.jsx
+++ pages/mainmenu.jsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import styles from '../Styles/MainMenu.module.css';
+import styles from '../src/Styles/mainmenu.module.css'
 
 export default function MainMenu() {
   return (
