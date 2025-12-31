c72983cd304922bf4f6306e4164b416e95083164
Suraj Raut <surajraut.dev@gmail.com>
2025-12-31 23:55:02 +0530

    Refreshed

---
+++ src/Components/AnimatedTitle.tsx
@@ -0,0 +1,39 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import Image from 'next/image';
+
+import styles from '../Styles/Animated.module.css';
+
+export default function AnimatedOverlay() {
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShow(false), 3000); // overlay lasts 3s
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <AnimatePresence>
+      {show && (
+        <motion.div
+          className={styles.overlay}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1.5 }}
+        >
+          <Image
+            src="/Logo.png" // logo in /public
+            alt="Midnight Echo Logo"
+            width={500}
+            height={500}
+            priority
+          />
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+}
+
+
---
+++ src/Components/header.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from '../Styles/Header.module.css';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+    <div className="logo">
+        <Image
+        src="/Logo.png"       // Path to image in /public folder
+        alt="Band logo"
+        width={80}
+        height={80}
+        />
+    </div>
+    <div>
+        <Link href="/MainMenu">
+            <Image
+            src="/LogoMenu.png"       // Path to image in /public folder
+            alt="Main Menu"
+            width={80}
+            height={80}
+            />
+        </Link>
+    </div>
+    </header>
+  );
+}
